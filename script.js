365448d43b63dad8c4096f3ffbb3aaa019d108bf
Lee Kyung Ryang <kyungryang@enuma.com>
2025-01-09 21:35:13 +0900

    파일 이동

---
+++ script.js
@@ -0,0 +1,158 @@
+$(document).ready(function() {
+	$('.box, .esc, .thumbnail').hide();
+
+	$('.esc').click(function() {
+		$('.box').hide();
+	});
+
+
+	$('h2').click(function() {
+		$('.esc').show();
+	})
+
+	$('.lt1').click(function() {
+		$('#lt1').toggle();
+	});
+
+	$('.lt2').click(function() {
+		$('#lt2').toggle();
+	});
+
+	$('.lt3').click(function() {
+		$('#lt3').toggle();
+	});
+
+	$('#t1').click(function() {
+		$('#c1').toggle();
+	});
+
+	$('#t2').click(function() {
+		$('#c2').toggle();
+	});
+
+	$('#t3').click(function() {
+		$('#c3').toggle();
+	});
+
+	$('#t4').click(function() {
+		$('#c4').toggle();
+	});
+
+	$('#t5').click(function() {
+		$('#c5').toggle();
+	});
+
+	$('#t6').click(function() {
+		$('#c6').toggle();
+	});
+
+	$('#t7').click(function() {
+		$('#c7').toggle();
+	});
+
+	$('#t8').click(function() {
+		$('#c8').toggle();
+	});
+
+	$('#t9').click(function() {
+		$('#c9').toggle();
+	});
+
+	$('#t10').click(function() {
+		$('#c10').toggle();
+	});
+
+	$('#t11').click(function() {
+		$('#c11').toggle();
+	});
+
+	$('#t12').click(function() {
+		$('#c12').toggle();
+	});
+
+	$('#t13').click(function() {
+		$('#c13').toggle();
+	});
+
+	$('#t14').click(function() {
+		$('#c14').toggle();
+	});
+
+	$('#t15').click(function() {
+		$('#c15').toggle();
+	});
+
+	$('#t16').click(function() {
+		$('#c16').toggle();
+	});
+
+	$('#t17').click(function() {
+		$('#c17').toggle();
+	});
+
+	$('#t18').click(function() {
+		$('#c18').toggle();
+	});
+
+	$('#t19').click(function() {
+		$('#c19').toggle();
+	});
+
+	$('#t20').click(function() {
+		$('#c20').toggle();
+	});
+
+	$('#t21').click(function() {
+		$('#c21').toggle();
+	});
+
+	$('#t22').click(function() {
+		$('#c22').toggle();
+	});
+
+	$('#t23').click(function() {
+		$('#c23').toggle();
+	});
+
+	$('#t24').click(function() {
+		$('#c24').toggle();
+	});
+
+	$('#t25').click(function() {
+		$('#c25').toggle();
+	});
+
+	$('#t26').click(function() {
+		$('#c26').toggle();
+	});
+
+	$('#t27').click(function() {
+		$('#c27').toggle();
+	});
+
+	$('#t28').click(function() {
+		$('#c28').toggle();
+	});
+
+	$('#t29').click(function() {
+		$('#c29').toggle();
+	});
+
+	$('#t30').click(function() {
+		$('#c30').toggle();
+	});
+
+	$('#t31').click(function() {
+		$('#c31').toggle();
+	});
+
+	$('#t32').click(function() {
+		$('#c32').toggle();
+	});
+
+	$('#t33').click(function() {
+		$('#c33').toggle();
+	});
+
+
+});
